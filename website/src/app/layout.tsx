09d682f3374d70f824b850de7bb96d9e1177073a
snehasish <snehasishcodes@gmail.com>
2025-02-11 22:52:31 +0530

    updated Metadata

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,11 +1,5 @@
-import type { Metadata } from "next";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
-
-export const metadata: Metadata = {
-	title: "Next App Template",
-	description: "Typescript, shadcn/ui, Framer Motion, GSAP, Next.js 15",
-};
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,4 +11,62 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
 			</body>
 		</html>
 	);
+}
+
+const metadataParams = {
+	title: "VSCode Status",
+	description: "Export your VSCode Activity to a REST API and access it anytime. Use cases: Display coding activity on your website, GitHub profile and more.",
+	images: ["/icon.png"],
+	icons: {
+		icon: "/icon.png",
+		shortcut: "/icon.png",
+		apple: "/icon.png"
+	},
+	siteName: "vscode.snehasish.xyz",
+	themeColor: "#FFFFFF",
+	url: "https://vscode.snehasish.xyz",
+	metabase: new URL("https://vscode.snehasish.xyz"),
+	keywords: ["vscode-status", "vscode status", "coding", "coding status", "coding activity", "vscode activity", "coding vscode", "vscode", "vscode api"],
+	twitter: {
+		card: "summary",
+		creator: "@snehasishcodes"
+	},
+	robots: {
+		index: false,
+		follow: true,
+		nocache: true,
+		googleBot: {
+			index: true,
+			follow: false,
+			noimageindex: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	}
+}
+
+export const metadata = {
+	title: metadataParams.title,
+	description: metadataParams.description,
+	metadataBase: metadataParams.metabase,
+	icons: metadataParams.icons,
+	openGraph: {
+		title: metadataParams.title,
+		description: metadataParams.description,
+		url: metadataParams.url,
+		siteName: metadataParams.siteName,
+		images: metadataParams.images,
+	},
+	twitter: {
+		title: metadataParams.title,
+		description: metadataParams.description,
+		creator: metadataParams.twitter.creator,
+		card: metadataParams.twitter.card,
+		images: metadataParams.images
+	}
+}
+
+export const viewport = {
+	themeColor: metadataParams.themeColor
 }
