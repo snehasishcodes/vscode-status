33f4d3348b96497129fe3a2ac802f4d7ef86437c
snehasishkun <snehasishcodes@gmail.com>
2025-02-22 19:31:08 +0530

    Remove webhook url from logger, also revoked it :0

--- api/src/lib/logger.ts
+++ api/src/lib/logger.ts
@@ -1,6 +1,11 @@
 import Webhook from "@lacerity/webhook";
+import dotenv from "dotenv";
 
-const wh = new Webhook("discord", "https://discord.com/api/webhooks/1342797872588128288/pQ4ymUGAYDXOA6tcgugIhE8gyr2fu3YdN0UGisxIP3-lTAEqDM-psJrB6JztPOrImD16");
+if (process.env.NODE_ENV !== "production") {
+    dotenv.config();
+}
+
+const wh = new Webhook("discord", process.env.LOGGER_WEBHOOK_URL!);
 
 const logger = (e: unknown) => {
     wh.sendError(e);
